0f64cf94d0b619f4a90f4151f9a272871f10f6ab
Your Name <you@example.com>
2024-11-08 14:38:59 +0430

    1

---
+++ script.js
@@ -0,0 +1,105 @@
+document.querySelector('.hidden').style.display = 'none'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///The JS WAY (1.2.3)(WRITE THROUGH FUNCTIONS)
+///1.2.3
+//console.log('Hello, Javascript');
+//value is the peace of information in JS that has different types 
+//Number Type + - * / ;
+//string type "This is a string" "this is \'hello\'" "hel" + "lo" ==>hello concatenation join two texts
+//program structure ==>statement ends ;
+//when the program executes all the instructions are read
+//console.log(12 / 0);//infinity
+//comments can't read //Word!
+/* use this type of comment to write comments on multiple lines */
+
+//Tasks
+// console.log("Eyas");//Elyas
+// console.log(14);//14
+// console.log(6 + 3);//9
+// console.log(6 - 3);//3
+// console.log(6 * 3);//18
+// console.log(6 / 3);//2
+// console.log(4 + 5);//9
+// console.log("4 + 5");//4 + 5
+// console.log("4" + "5");//45
+//write functions
