480636bb80c2c05bbf0039486febe0a5b69ac65f
Your Name <you@example.com>
2024-11-12 10:54:31 +0430

    header

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-document.querySelector('.hidden').style.display = 'none'
+// document.querySelector('.hidden').style.display = 'none'
+
+//Code Academy Intro to Javascript
+//1
+//What is Javascript: it is the web browser language
+//Console key word: it is the place where developer can see the errors ...
+//In JS console.log() keyWord refers to collection of data and an object with this we can see everything in the console EX: console.log(5);
+//Comments: it is like putting text for other developers and for our selfs it is a peace of information that does not executes at all but help us to write better codes
+//code comments in JS: 1 // 2./* */ Ex: //Open the line , /*hey*/ the // use for single lines and the /* */ use for multi and middle lines of codes
+//Data Types: we have different kinds of data in programming 
+//1. Number // 1.2 4 it can hod decimal numbers as well EX: console.log(5.5);
+//2. String // "Texts" 'Texts' EX: console.log("hello how are you")
+//3. Bigint // too large numbers 1234567801n  EX:console.log(BigInt(3333333333333333336446666));
+//4. Boolean // something true or false EX: let ali = true;
+//5.null //it is its own value and it means that there is no value EX: let Hello = null//It is an object
+//6.undefined // it is it's own value and means that a value is not declared yet EX: let Hello = undefined;
+//7.Symbol 8. object///are new
+//Arithmetic Operators: (1. + // Addition console.log(4 + 4)// 8), (2. - //subtraction console.log(4 - 4)// 0), (3. * // Multiplication console.log(5 * 5)// 25), (4. / ==> division console.log(4 / 4)// 1), (5. % Remainder or Modules console.log(12 % 2)// 0)
+//String Concatenation: With plus operator we can append one peace of text to another EX: console.log("He" + " llo") for space we can space between the strings also we can use a empty string to spacing ''
+//Properties// it use most for constant of a page like in string we have a property .length that calculates the amount of text letters in a string EX: console.log("Hello" .length)// 5 we can get access to this with (.) dot//action
+//Methods: Same as the property to access the data the syntax to call .method name(value)//quality(1. .toUpperCase() 2.startsWith("H")//true)console.log("hello" .toUpperCase()//HELLO 3. .trim()//remove the whitespace from the beginning and end)
+//Built-in-Objects // it is about the console it self we have math objects : 
+//1. Math.random() a number between 0 and 1 like decimal EX: console.log(Math.random())//a random number between 0 and 5 also we can use * to find a number EX: Math.random() * 5;//an Num between the 0 and 5  it  means that if we wanna choose a number which is not 0 and 1 we use the * operator  
+//2.Math.floor()//It takes all the decimal numbers and make it like the nearest num EX: console.log(Math.floor(Math.random()))//a random num without decimal EX: console.log(Math.floor(Math.random() * 50))//Between 0 and 50 without the decimal value 
+//3. Math.ceil()//this use to roundup and show the nearest number 44.8//50 because it is the nearest number
+//4. Number.isInteger // it is a math object that check if the Num is integer or no EX: console.log(Number.isInteger(50))//true we use Number object
 
 
